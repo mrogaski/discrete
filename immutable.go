a4ee5ce2abf605690551b0daea68367969c6d7c8
Mark Rogaski <mrogaski@pobox.com>
2022-10-11 23:33:38 -0400

    Added Contains, Size, and Members methods.

--- immutable.go
+++ immutable.go
@@ -1,8 +1,52 @@
 // Package set implements generic set types.
+//
+// Member type must be comparable.
 package set
 
-type ImmutableSet[T comparable] struct{}
+// An ImmutableSet is a collection of unique elements.
+type ImmutableSet[T comparable] struct {
+	members map[T]nothing
+}
 
-func NewImmutableSet[T comparable](members ...[]T) *ImmutableSet[T] {
-	return nil
+type nothing struct{}
+
+// NewImmutableSet returns an immutable set containing any member elements passed as arguments.
+// Any duplicate arguments will be ignored.
+func NewImmutableSet[T comparable](members ...T) *ImmutableSet[T] {
+	var nihil nothing
+
+	seq := make(map[T]nothing, len(members))
+
+	for _, elem := range members {
+		seq[elem] = nihil
+	}
+
+	return &ImmutableSet[T]{members: seq}
+}
+
+// Contains returns true if the given element is a member of the set.
+func (s *ImmutableSet[T]) Contains(elem T) bool {
+	_, ok := s.members[elem]
+
+	return ok
+}
+
+// Size returns a count of the number of members contained in the set.
+func (s *ImmutableSet[T]) Size() int {
+	return len(s.members)
+}
+
+// Members returns a slice containing the set members.
+// There is no guaranteed ordering.
+func (s *ImmutableSet[T]) Members() []T {
+	result := make([]T, len(s.members))
+
+	i := 0
+
+	for elem := range s.members {
+		result[i] = elem
+		i++
+	}
+
+	return result
 }
--- immutable_test.go
+++ immutable_test.go
@@ -1,6 +1,7 @@
 package set_test
 
 import (
+	"sort"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -25,8 +26,104 @@ func TestNewImmutableSet(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 
-			s := set.NewImmutableSet(tt.members)
+			s := set.NewImmutableSet(tt.members...)
+
 			assert.IsType(t, &set.ImmutableSet[rune]{}, s)
 		})
 	}
+}
+
+func TestImmutableSet_Contains(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name    string
+		members []rune
+		elem    rune
+		want    bool
+	}{
+		{name: "empty", members: []rune{}, elem: 'A', want: false},
+		{name: "hit", members: []rune{'U', 'A'}, elem: 'A', want: true},
+		{name: "miss", members: []rune{'U', 'A'}, elem: 'Z', want: false},
+		{name: "hit only member", members: []rune{'A'}, elem: 'A', want: true},
+		{name: "miss only member", members: []rune{'A'}, elem: 'Z', want: false},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			s := set.NewImmutableSet(tt.members...)
+
+			assert.Equal(t, tt.want, s.Contains(tt.elem))
+		})
+	}
+}
+
+func TestImmutableSet_Size(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name    string
+		members []rune
+		want    int
+	}{
+		{name: "empty", members: []rune{}, want: 0},
+		{name: "1 member", members: []rune{'A'}, want: 1},
+		{name: "2 member", members: []rune{'A', 'B'}, want: 2},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			s := set.NewImmutableSet(tt.members...)
+
+			assert.Equal(t, tt.want, s.Size())
+		})
+	}
+}
+
+func TestImmutableSet_Members(t *testing.T) {
+	t.Parallel()
+
+	sequence := make([]rune, 0)
+	for i := 0x20; i < 0x80; i++ {
+		sequence = append(sequence, rune(i))
+	}
+
+	tests := []struct {
+		name    string
+		members []rune
+	}{
+		{name: "empty", members: []rune{}},
+		{name: "1 member", members: []rune{'A'}},
+		{name: "sequence", members: sequence},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			s := set.NewImmutableSet(tt.members...)
+
+			assert.Equal(t, sorted(tt.members), sorted(s.Members()))
+		})
+	}
+}
+
+func sorted(input []rune) []rune {
+	result := make([]rune, len(input))
+	copy(result, input)
+
+	sort.Slice(input, func(i, j int) bool {
+		return input[i] < input[j]
+	})
+
+	return input
 }
